6cf72c376159818f7a14d78384e70fe09bc9d980
Tim Jansma <tjansma@gmail.com>
2024-05-24 10:19:10 +0200

    Introduced IBoard interface (ABC)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 """Conway's game of life."""
 
+from abc import ABCMeta, abstractmethod
 import copy
 import sys
 import time
@@ -13,8 +14,41 @@ MAIN_WINDOWS_SIZE = (CELL_COUNT_HORIZONTAL * CELL_SIZE,
                      CELL_COUNT_VERTICAL * CELL_SIZE)
 NEXT_STEP_INTERVAL = 0.25
 
+class IBoard(metaclass=ABCMeta):
+    @classmethod
+    def __subclasshook__(cls, subclass: type) -> bool:
+        return (hasattr(subclass, "toggle") and callable(subclass.toggle) and
+                hasattr(subclass, "next_step") and callable(subclass.next_step)
+        )     
 
-class Board:        
+    @abstractmethod
+    def toggle(self, x: int, y: int) -> None:
+        """Toggle live/dead status of cell at specified coordinates.
+
+        Args:
+            x (int): X coordinate
+            y (int): Y coordinate
+        """
+        raise NotImplementedError
+    
+    @abstractmethod
+    def next_step(self):
+        """Generate next step according to Conway's game of life rules:
+        1. Any live cell with fewer than two live neighbors dies, as if by
+           underpopulation.
+        2. Any live cell with two or three live neighbors lives on to the next
+           generation.
+        3. Any live cell with more than three live neighbors dies, as if by
+           overpopulation.
+        4. Any dead cell with exactly three live neighbors becomes a live
+           cell, as if by reproduction.
+
+        Returns:
+            IBoard: board object with new generations.
+        """
+        raise NotImplementedError
+
+class Board(IBoard):        
     def __init__(self, grid: list[list[bool]] = None):
         if grid is None:
             self.grid = [[False] * CELL_COUNT_HORIZONTAL for i in
@@ -31,7 +65,7 @@ class Board:
         """
         self.grid[y][x] = not self.grid[y][x]
 
-    def get_neighbour_count(self, x: int, y: int) -> int:
+    def _get_neighbour_count(self, x: int, y: int) -> int:
         """Count number of neighbours of cell at specified coordinates.
 
         Args:
@@ -49,7 +83,7 @@ class Board:
 
         return neighbour_count
 
-    def next_step(self):
+    def next_step(self) -> IBoard:
         """Generate next step according to Conway's game of life rules:
         1. Any live cell with fewer than two live neighbors dies, as if by
            underpopulation.
@@ -61,12 +95,12 @@ class Board:
            cell, as if by reproduction.
 
         Returns:
-            Board: board object with new generations.
+            IBoard: board object with new generations.
         """
         next_board = Board()
         for y in range(len(self.grid)):
             for x in range(len(self.grid[y])):
-                neighbour_count = self.get_neighbour_count(x, y)
+                neighbour_count = self._get_neighbour_count(x, y)
                 if self.grid[y][x]:
                     if neighbour_count < 2:
                         next_board.grid[y][x] = False
@@ -79,6 +113,7 @@ class Board:
                         next_board.grid[y][x] = True
 
         return next_board
+
 
 def draw_board(surface: pygame.Surface, board: Board) -> None:
     """Draw Conway board on given pygame Surface.
