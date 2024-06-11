19b2b35be21b51541c58de4e0511faf88242ac5f
Tim Jansma <tjansma@gmail.com>
2024-06-11 12:45:44 +0200

    Initial complete game.

--- main.py
+++ main.py
@@ -20,15 +20,11 @@ class PygameBoard:
         self._updated = False
 
     def draw_board(self) -> None:
-        """Draw Conway board on given pygame Surface.
-
-        Args:
-            surface (pygame.Surface): surface to draw on.
-            board (Board): Conway's game of life board.
+        """Draw Conway board on registered pygame Surface.
         """
         if self._updated:
-            CELL_COLOR = (255, 255, 255) # White
-            BG_COLOR = (0, 0, 0) # Black
+            CELL_COLOR = (255, 255, 255)    # White
+            BG_COLOR = (0, 0, 0)            # Black
 
             window = pygame.Rect(0, 0, MAIN_WINDOW_SIZE[0], MAIN_WINDOW_SIZE[1])
             pygame.draw.rect(self.surface, BG_COLOR, window)
@@ -47,30 +43,51 @@ class PygameBoard:
         self.board = self.board.next_step()
         self._updated = True
 
+    def toggle(self, x, y):
+        self.board.toggle(x, y)
+        self._updated = True
+
+    def clear(self):
+        x_cells = len(self.board.grid[0])
+        y_cells = len(self.board.grid)
+        self.board = conway.Board(x_cells, y_cells)
+        self._updated = True
+
 def main():
     """Execute main function."""
     screen = pygame.display.set_mode(MAIN_WINDOW_SIZE)
 
     board = conway.Board(size_x=CELL_COUNT_HORIZONTAL, size_y=CELL_COUNT_VERTICAL)
-    board.toggle(0, 1)
-    board.toggle(1, 2)
-    board.toggle(2, 0)
-    board.toggle(2, 1)
-    board.toggle(2, 2)
     pg_board = PygameBoard(board, screen)
 
-    pygame.time.set_timer(pygame.USEREVENT, NEXT_STEP_INTERVAL_IN_MS)
-
+    running = False
     exit_game = False
     generation = 0
     while not exit_game:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                exit_game = True
-            elif event.type == pygame.USEREVENT:
+            if event.type == pygame.USEREVENT:
                 generation += 1
                 pg_board.next_step()
                 pg_board.draw_board()
+            elif event.type == pygame.QUIT:
+                exit_game = True
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                cell_x = mouse_x // CELL_SIZE
+                cell_y = mouse_y // CELL_SIZE
+                pg_board.toggle(cell_x, cell_y)
+                pg_board.draw_board()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    if not running:
+                        pygame.time.set_timer(pygame.USEREVENT, NEXT_STEP_INTERVAL_IN_MS)
+                        running = True
+                    else:
+                        pygame.time.set_timer(pygame.USEREVENT, 0)
+                        running = False
+                elif event.key == pygame.K_BACKSPACE:
+                    pg_board.clear()
+                    pg_board.draw_board()
 
         pygame.display.update()
 
